2070c64551bddda912330de6329243265ab645db
bosmat-haifa <57598261+bosmat-haifa@users.noreply.github.com>
2020-01-29 21:05:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+// const ejs = require("ejs");
+const express = require("express");
+const app = express();
+
+// app.engine("html", ejs.renderFile);
+// app.set("views", "public");
+// app.set("views engine", "html");
+app.listen(process.env.PORT || 8080, () => {console.log(process.env.PORT)});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    var data = fs.readFileSync("database.json", "utf-8");
+    var hashes = JSON.parse(data)["hashes"];
+    var hash = hashes[Math.floor(Math.random() * hashes.length)];
+    fs.writeFileSync("public/script.js", `window.location.hash = "${hash}";`);
+    // res.render("index.html");
+    res.end();
+});
+
+app.get("/favicon.ico", (req, res) => {
+    res.end();
+});
